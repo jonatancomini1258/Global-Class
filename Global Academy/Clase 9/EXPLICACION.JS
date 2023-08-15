b87e24b5266838e1fd18535f2674a651186213d0
Johnny <116278988+jonatancomini1258@users.noreply.github.com>
2023-08-15 11:01:32 -0300

    Add files via upload

---
+++ Global Academy/Clase 9/EXPLICACION.JS
@@ -0,0 +1,21 @@
+/* Comandos basicos de git
+
+-git clone {url del repositorio} : es la forma de clonar en mi pc, el repositorio que esta en github
+
+Pasos a seguir para subir codigo nuevo a git:
+-git add . : Esto es para agregar los cambios efectuados al commit
+-git commit -m "aca va un comentario" : Es para armar el commit
+-git push : Es para enviar el commit
+
+Pasos a seguir para actualizar mi local, con respecto al repo:
+-git pull : Es para traer los cambios de github al local
+-git pull origin main : me trae los cambios de main
+
+Como cambiar de rama
+-git checkout {nombre de la rama} : Para cambiar la rama en la que estoy parado
+
+Como agrego cambios de una rama a main?
+1. Subo los cambios
+2. Creo el pull request
+3. lo acepto y se mergea
+*/
